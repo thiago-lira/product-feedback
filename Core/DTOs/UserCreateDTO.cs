f1f4955293f6fc975a1d1dcc747facc2c81dddd9
Thiago Lira <lira_thiago@msn.com>
2022-01-01 19:37:21 -0300

    Set user create DTO with required fields

--- Core/DTOs/UserCreateDTO.cs
+++ Core/DTOs/UserCreateDTO.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 using Core.Models;
 
 namespace Core.DTOs
 {
     public class UserCreateDTO
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Username { get; set; }
 
         public User ToModel()
