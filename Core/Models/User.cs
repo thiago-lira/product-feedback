02040c9d0c0adabaa1b4ae37facc683912a8dc2d
Thiago Lira <lira_thiago@msn.com>
2022-01-01 19:20:30 -0300

    Set user model fields as required

--- Core/Models/User.cs
+++ Core/Models/User.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Core.Models
 {
     public class User : ModelBase
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Username { get; set; }
     }
 }
